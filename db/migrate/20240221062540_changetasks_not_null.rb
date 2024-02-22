cd8782e4283c06cbd74007843cde1b5dd08e60ca
kasagata <k.sagata.1207@gmail.com>
2024-02-22 12:01:25 +0900

    step1

---
+++ db/migrate/20240221062540_changetasks_not_null.rb
@@ -0,0 +1,5 @@
+class ChangetasksNotNull < ActiveRecord::Migration[6.1]
+  def change
+    change_column :tasks, :title, :string, null: false
+  end
+end
---
+++ db/migrate/20240221062803_changetaskscontents_not_null.rb
@@ -0,0 +1,5 @@
+class ChangetaskscontentsNotNull < ActiveRecord::Migration[6.1]
+  def change
+    change_column :tasks, :content, :text, null: false
+  end
+end
