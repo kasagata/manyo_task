32362decd293b4ee0eb6fca3c875be9f342eb24f
kasagata <k.sagata.1207@gmail.com>
2024-02-28 17:36:07 +0900

    課題のフィードバック#18の修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,15 @@
 class Task < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
+    validates :deadline_on, presence: true
+    validates :priority, presence: true
+    validates :status, presence: true
     enum priority:[:low, :middle, :high]
     enum status: [:not_started, :in_progress, :completed]
     
     #ソート
     scope :sort_deadline_on, -> { order(:deadline_on) }
-    scope :sort_priority, -> { order(priority: :desc) }
+    scope :sort_priority, -> { order(priority: :desc, created_at: :desc) }
 
     #検索
     scope :search, -> (search_params) do
